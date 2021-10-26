c86c06741aa151d450f804d30527c2c162958ec6
Florian Chaudret <florian.chaudret@gmail.com>
2021-10-26 20:43:23 +0200

    wip 7

--- src/infra/articles/articles.module.ts
+++ src/infra/articles/articles.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CqrsModule } from '@nestjs/cqrs';
 import { ArticlesCommandHandlers } from './command';
 import { ArticleAggregateConstructorFactoryProvider } from '@infra/articles/article.factory-provider';
 import { ArticlesEventHandlers } from '@infra/articles/event';
 import { FEventBusModule } from '@infra/f-event-bus/f-event-bus.module';
 
 @Module({
-  imports: [CqrsModule, FEventBusModule],
+  imports: [FEventBusModule],
   providers: [ArticleAggregateConstructorFactoryProvider, ...ArticlesCommandHandlers, ...ArticlesEventHandlers],
 })
 export class ArticlesModule {}
